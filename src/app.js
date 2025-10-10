33b91bffdc71c98b139c8dde84d889f734ffb0b9
Nathan Bitoun <nathan.bitoun@etu.unice.fr>
2025-10-10 12:50:21 +0200

    Carte en cours

--- src/app.js
+++ src/app.js
@@ -56,4 +56,18 @@ app.use((err, req, res, next) => {
     next(err);
 });
 app.use(errorHandler());
+const helmet = require("helmet");
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://unpkg.com"],
+      styleSrc: ["'self'", "https://unpkg.com"],
+      imgSrc: ["'self'", "data:", "https://*.tile.openstreetmap.org"],
+    },
+  })
+);
+
+
 module.exports = app;

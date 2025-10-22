d54210641dfc59b24e12d1f399dfb110dc580c2e
Hugo <hugomater@orange.fr>
2025-10-22 15:40:44 +0200

    feat: middleware to block if user are banned

--- src/routes/review.js
+++ src/routes/review.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const review = require('../controllers/review.js');
 
 const authMiddleware = require('../middlewares/auth.js');
+const activeMiddleware = require('../middlewares/active.js');
 const verifyMiddleware = require('../middlewares/verify.js');
 
 router.use('/locations/:lid/reviews', verifyMiddleware.verifyLocationID);
@@ -10,9 +11,9 @@ router.use('/review/:rid',authMiddleware.verifyTokenPresence, verifyMiddleware.v
 router.use('/manage/review/:rid', authMiddleware.verifyTokenPresence, authMiddleware.verifyTokenAdmin, verifyMiddleware.verifyReviewID);
 
 router.get('/locations/:lid/reviews', review.getReview);
-router.post('/locations/:lid/reviews', authMiddleware.verifyTokenPresence, review.addReview);
+router.post('/locations/:lid/reviews', authMiddleware.verifyTokenPresence, activeMiddleware.verifyUserBan, review.addReview);
 
-router.put('/review/:rid', review.modifyReview);
+router.put('/review/:rid', activeMiddleware.verifyUserBan, review.modifyReview);
 router.delete('/review/:rid', review.deleteReview);
 
 router.get('/manage/review/:rid', review.getReviewByAdmin);
